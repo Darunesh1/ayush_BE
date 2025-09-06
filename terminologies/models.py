7abc088bdafc368369130b71ccc96d840b84bb66
darunesh1 <sathyarajasekar5873@gmail.com>
2025-09-06 20:20:39 +0530

    some minar chnages and added ruff

--- terminologies/models.py
+++ terminologies/models.py
@@ -1,42 +1,41 @@
 from django.db import models
 
+
 class BaseNamasteModel(models.Model):
     code = models.CharField(max_length=50, unique=True)  # Unique code (e.g., NAMC_CODE)
     description = models.TextField(null=True, blank=True)
     english_name = models.CharField(max_length=255, null=True, blank=True)
-    
 
     class Meta:
         abstract = True
-        
+
     def __str__(self):
-        return f"{self.code} - {self.term}"
+        return f"{self.code} - {self.english_name}"
 
 
 class AyurvedhaModel(BaseNamasteModel):
     hindi_name = models.CharField(max_length=255, null=True, blank=True)
     diacritical_name = models.CharField(max_length=255, null=True, blank=True)
-    
+
 
 class SiddhaModel(BaseNamasteModel):
     tamil_name = models.CharField(max_length=255, null=True, blank=True)
     romanized_name = models.CharField(max_length=255, null=True, blank=True)
     reference = models.TextField(null=True, blank=True)
 
+
 class UnaniModel(BaseNamasteModel):
     arabic_name = models.CharField(max_length=255, null=True, blank=True)
     romanized_name = models.CharField(max_length=255, null=True, blank=True)
     reference = models.TextField(null=True, blank=True)
-    
 
-from django.db import models
 
 class ICDClassKind(models.Model):
     name = models.CharField(max_length=50, unique=True)
     description = models.TextField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.name}"
 
 
 class ICD11Term(models.Model):
@@ -44,7 +43,9 @@ class ICD11Term(models.Model):
     linearization_uri = models.URLField(max_length=500, null=True, blank=True)
     code = models.CharField(max_length=50, null=True, blank=True)
     title = models.CharField(max_length=255)
-    class_kind = models.ForeignKey(ICDClassKind, on_delete=models.PROTECT, null=True, blank=True)
+    class_kind = models.ForeignKey(
+        ICDClassKind, on_delete=models.PROTECT, null=True, blank=True
+    )
     depth_in_kind = models.PositiveIntegerField(null=True, blank=True)
     is_residual = models.BooleanField(default=False)
     primary_location = models.CharField(max_length=255, null=True, blank=True)
@@ -56,6 +57,4 @@ class ICD11Term(models.Model):
     version_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
-        return f"{self.code} - {self.title}" if self.code else self.title
-
-
+        return f"{self.code} - {self.title}" if self.code else f"{self.title}"
