338f23282c80ce17803ad7d4fbda51fc6512ffc8
darunesh1 <sathyarajasekar5873@gmail.com>
2025-09-10 13:48:24 +0530

    changed the name

--- terminologies/urls.py
+++ terminologies/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import ayurvedha_fuzzy_search
 
 urlpatterns = [
-    path("api/ayurveda/search/", ayurvedha_fuzzy_search, name="ayurvedha-fuzzy-search"),
+    path("ayurveda/search/", ayurvedha_fuzzy_search, name="ayurvedha-fuzzy-search"),
 ]
 
