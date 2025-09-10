af43fd08dd0874dbe64ba43072fc9984d6e574b3
darunesh1 <sathyarajasekar5873@gmail.com>
2025-09-10 13:46:58 +0530

    changed the urls and how it is handeleed

--- config/urls.py
+++ config/urls.py
@@ -14,18 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/fhir/", include("fhir.urls")),
-    path("api/auth/", include("auth_abha.urls")),
-    path("api/analytics/", include("analytics.urls")),
-    path("api/terminologies/", include("terminologies.urls")),
+    path("fhir/", include("fhir.urls")),
+    path("auth/", include("auth_abha.urls")),
+    path("analytics/", include("analytics.urls")),
+    path("terminologies/", include("terminologies.urls")),
 ]
 
 
