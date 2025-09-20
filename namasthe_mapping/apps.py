a150c8c00d72006bd2a5c91403d85b1e72f45c6d
darunesh1 <sathyarajasekar5873@gmail.com>
2025-09-20 22:50:56 +0530

    migrations for the mapping

--- namasthe_mapping/apps.py
+++ namasthe_mapping/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class NamastheMappingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'namasthe_mapping'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "namasthe_mapping"
