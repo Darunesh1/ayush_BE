0c1d0d6002ee6ce1298350cbb2f8de65c20d78c1
darunesh1 <sathyarajasekar5873@gmail.com>
2025-09-23 09:12:07 +0530

    updated how the apps are loaded

--- namasthe_mapping/__init__.py
+++ namasthe_mapping/__init__.py
@@ -1,15 +1,17 @@
 # namasthe_mapping/__init__.py
-# Ensure tasks are imported
+
+# Remove the automatic import - this is causing the error
+# Let Celery's autodiscovery handle the tasks instead
 
 default_app_config = "namasthe_mapping.apps.NamasteMappingConfig"
 
+# Remove this function and call - it's causing AppRegistryNotReady
+# def import_tasks():
+#     try:
+#         from . import tasks
+#     except ImportError:
+#         pass
 
-# Force import tasks when module is imported
-def import_tasks():
-    try:
-        from . import tasks
-    except ImportError:
-        pass
+# import_tasks()  # Remove this line
 
-
-import_tasks()
+# Celery will automatically discover tasks, so no manual import needed
